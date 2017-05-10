6c4d2dc8fe1193c1ef1c8683e380391a2c36c029
Mark <lemay@bu.edu>
2017-05-10 11:51:10 -0400

    migrate more to kt

---
+++ src/main/kotlin/com/atslangplugin/psi/ATSElementType.kt
@@ -0,0 +1,8 @@
+package com.atslangplugin.psi
+
+import com.atslangplugin.ATSLanguage
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.annotations.NonNls
+
+
+class ATSElementType(@NonNls debugName: String) : IElementType(debugName, ATSLanguage.INSTANCE)
---
+++ src/main/kotlin/com/atslangplugin/psi/ATSTokenType.kt
@@ -0,0 +1,12 @@
+package com.atslangplugin.psi
+
+import com.atslangplugin.ATSLanguage
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.annotations.NonNls
+
+
+class ATSTokenType(@NonNls debugName: String) : IElementType(debugName, ATSLanguage.INSTANCE) {
+    override fun toString(): String {
+        return "ATSTokenType." + super.toString()
+    }
+}
